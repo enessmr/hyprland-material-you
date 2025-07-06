10feda31c4b79dda9d39376297e0b4135b1fa89a
Danik <retikadni@gmail.com>
2025-07-06 18:33:07 +0200

    New row: color

--- hypryou/src/modules/settings/appearance.py
+++ hypryou/src/modules/settings/appearance.py
@@ -1,12 +1,9 @@
 from utils import colors
-from src.modules.settings.base import RowTemplate, SwitchRowTemplate
-from src.modules.settings.base import SettingsBoolRow
+from src.modules.settings.base import SwitchRowTemplate
+from src.modules.settings.base import SettingsBoolRow, SettingsTextRow
 from src.modules.settings.base import Category
 import typing as t
 from repository import gtk
-import src.widget as widget
-from utils import sync_debounce
-from config import Settings
 
 
 class ToggleDarkMode(SwitchRowTemplate):
@@ -31,61 +28,6 @@ class ToggleDarkMode(SwitchRowTemplate):
         super().destroy()
 
 
-class OpacityRow(RowTemplate):
-    __gtype_name__ = "SettingsOpacityRow"
-
-    def __init__(self) -> None:
-        self.settings = Settings()
-        super().__init__(
-            "UI Opacity",
-            "Changes opacity of UI (minimum: 85%)",
-            css_classes=("opacity-row",)
-        )
-
-        self.entry_box = gtk.Box(
-            css_classes=("entry-box",)
-        )
-        self.entry = gtk.Entry(
-            css_classes=("entry",),
-            max_length=3,
-            max_width_chars=3,
-            halign=gtk.Align.END
-        )
-        self.percent = widget.Icon("percent")
-        self.entry_box.append(self.entry)
-        self.entry_box.append(self.percent)
-        self.append(self.entry_box)
-
-        self.debounced_text_changed = sync_debounce(500)(self.text_changed)
-        self.entry_handler = (
-            self.entry.connect("notify::text", self.debounced_text_changed)
-        )
-        self.settings_handler = self.settings.watch(
-            "opacity", self.setting_updated
-        )
-
-    def setting_updated(self, new_value: float) -> None:
-        value = str(round(new_value * 100))
-        if self.entry.get_text() != value:
-            self.entry.handler_block(self.entry_handler)
-            self.entry.set_text(value)
-            self.entry.handler_unblock(self.entry_handler)
-
-    def destroy(self) -> None:
-        super().destroy()
-        self.entry.disconnect(self.entry_handler)
-
-    def text_changed(self, *args: t.Any) -> None:
-        text = self.entry.get_text()
-        if not text.isdigit():
-            value = str(round(self.settings.get("opacity") * 100))
-            self.entry.set_text(value)
-            return
-
-        value = min(max(float(text) / 100, 0.85), 1.0)
-        self.settings.set("opacity", value)
-
-
 class AppearancePage(gtk.ScrolledWindow):
     __gtype_name__ = "SettingsAppearancePage"
 
@@ -102,6 +44,13 @@ class AppearancePage(gtk.ScrolledWindow):
         self.children = (
             Category("Colors"),
             ToggleDarkMode(),
+            SettingsTextRow(
+                "Color",
+                "Use different color for UI instead of wallpapers' one",
+                "color",
+                "tag",
+                max_length=6
+            ),
 
             Category("Effects"),
             SettingsBoolRow(
@@ -114,7 +63,16 @@ class AppearancePage(gtk.ScrolledWindow):
                 "Adds xray effect to blur",
                 "blur_xray"
             ),
-            OpacityRow(),
+            SettingsTextRow(
+                "UI Opacity",
+                "Changes opacity of UI (minimum: 85%)",
+                "opacity",
+                right_icon="percent",
+                max_length=3,
+                transform_fn=lambda v: str(round(float(v) * 100)),
+                transform2_fn=lambda v: min(max(float(v) / 100, 0.85), 1.0),
+                test_text=lambda v: v.isdigit()
+            ),
 
             Category("Layout"),
             SettingsBoolRow(
